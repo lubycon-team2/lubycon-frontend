83626f0eb1ad51844e77e5f0afc4ad1fd953d2ac
ssminji <ssong758@gmail.com>
2020-10-16 15:56:18 +0900

    fix login

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -15,24 +15,25 @@ class Login extends Component {
 
     handleLogin(e) {
         let sns = e.currentTarget.value;
-        
-        return this.props.loginRequest(sns).then(
-            () => {
-                if(this.props.status === "SUCCESS" ) {
-                    // create session data
-                    let loginData = {
-                        isLoggedIn: true,
-                        accessToken: this.props.accessToken,
-                    };
-                    document.cookie = 'key=' + btoa(JSON.stringify(loginData)); // save session data in cookie 
-                    console.log(this.props.accessToken);
-                    browserHistory.push('/'); 
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        )
+        let api = 'https://api.partying.cf/oauth2/authorization/' + sns;
+        window.location.replace(api);
+        // return this.props.loginRequest(sns).then(
+        //     () => {
+        //         if(this.props.status === "SUCCESS" ) {
+        //             // create session data
+        //             let loginData = {
+        //                 isLoggedIn: true,
+        //                 accessToken: this.props.accessToken,
+        //             };
+        //             document.cookie = 'key=' + btoa(JSON.stringify(loginData)); // save session data in cookie 
+        //             console.log(this.props.accessToken);
+        //             browserHistory.push('/'); 
+        //             return true;
+        //         } else {
+        //             return false;
+        //         }
+        //     }
+        // )
     }
 
     render() {
