9160d519e6aa3c4347d0b9a10335a3299eb72943
ssMinji <ssong758@gmail.com>
2020-10-22 17:14:53 +0900

    fix minor bugs

--- src/containers/new_party/SetPartyName.js
+++ src/containers/new_party/SetPartyName.js
@@ -1,6 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-/* eslint-disable jsx-a11y/alt-text */
-/*eslint-disable-next-line*/
 import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
 
