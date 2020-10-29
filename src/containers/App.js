be59d9c6eeec2d6fc09e4a39f67d1f5223b1dae2
ssMinji <ssong758@gmail.com>
2020-10-29 10:09:56 +0900

    check urlpath

--- src/containers/App.js
+++ src/containers/App.js
@@ -40,7 +40,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        {this.state.urlPath === '/login' || this.state.urlPath === '/party/new'? undefined :<Header isLoggedIn={this.props.status.isLoggedIn}
+        {location.urlPath === '/login' || location.urlPath === '/party/new'? undefined :<Header isLoggedIn={this.props.status.isLoggedIn}
                 onLogout={this.handleLogout} />}
         {this.props.children}
       </div>
